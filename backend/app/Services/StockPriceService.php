47ff7bee97d0ab5b9c43690290dcff65c6592124
Aleksandar Ustic <aleksandar.ustic@digitalinsurance.io>
2024-03-07 23:21:54 +0100

    Finishing with work

--- backend/app/Services/StockPriceService.php
+++ backend/app/Services/StockPriceService.php
@@ -136,12 +136,6 @@ class StockPriceService
     {
         $sub = TickerStockPrice::select(DB::raw('*, lag(price, 1,0) over (PARTITION BY ticker_id ORDER BY date ASC) as previous_price'))->orderBy('date', 'DESC');
 
- //        $sub = DB::table('ticker_stock_prices')->selectRaw('*, lag(price, 1,0) over (PARTITION BY ticker_id ORDER BY date ASC) as previous_price')->orderBy('date', 'DESC');
-//
-//        $report = DB::table( DB::raw("({$sub->toSql()}) as T") )
-//            ->mergeBindings($sub)
-//            ->selectRaw('*, ROUND(((price-previous_price)/previous_price)*100) as percentage_change')->get();
-//
         return TickerStockPrice::query()->from( DB::raw("({$sub->toSql()}) as T") )
             ->selectRaw('*, ROUND(((price-previous_price)/previous_price)*100) as percentage_change')->get();
     }
